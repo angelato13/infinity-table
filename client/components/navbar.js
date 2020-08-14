c6e7518a34cea0959c1123fc7b0122f6671583d2
Angela Ratto <angelaratto@gmail.com>
2020-08-14 18:30:46 -0400

    updated name to Infinity Table

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>Infinity Mirror</h1>
+    <h1>Infinity Table</h1>
     <nav>
       {isLoggedIn ? (
         <div>
